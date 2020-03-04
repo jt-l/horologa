b1d26ec5301357db1cccade20995973f70457b05
james <jamesleahy314@gmail.com>
2020-03-04 00:53:47 -0800

    inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Horologa
+=
+
+Determine the frame length to be used in a static clock-driven scheduler. 
+
+Given a set of tasks where each task contains a phase, period, execution time, and relative deadline horologa will determine if there is a valid frame length that can be used for the provided set of tasks. 
+
+A static clock-driven schedulers are applicable to deterministic systems in which the parameters of all periodic tasks are known a prior. A static schedule can be computed in advance. 
+
+To simplify implementation of these shedulers often it is recommended to impose structure, this is done by introducing periodic intervals called frames. Scheduling decisions are to be made at each frame. This gives two main benefits; the scheduler can check for overruns/missed deadlines at the end of each frame and the scheduler can use a periodic clock interrupt rather than a programable timer.
+
+This program determines a valid frame length by searching frames which statisfy all three of the following constraints: 
+
+1. frame length >= max(e_1, e_2, ... e_n)
+2. There must exist some p_i such that frame length % p_i == 0
+3. 2*frame length - gcd(p_i, frame length) > D_i for i = 1 ... n
+
+If the program cannot find a valid frame length this means that the given tasks cannot satify all three constraints at once. In this case you can attempt to generate new tasks by paritioning a task with large execution time into smaller sub tasks.
+
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "encoding/json"
+  "os"
+)
+
+/* A periodic task is defined to have:
+phase: release time of the task
+period: task needs to be scheduled every period and within the frame
+execution_time: cycles that the task takes to execute
+relative_deadline: time from the phase in which in the task must complete
+*/
+type Task struct {
+  Phase int `json:"phase"`
+  Period int `json:"period"`
+  ExecutionTime int `json:"executionTime"`
+  RelativeDeadline int `json:"relativeDeadline"`
+}
+
+type Tasks struct {
+  T []Task `json:"tasks"`
+}
+
+// helper function to determine the gcd
+func gcd(a int, b int) int {
+  for a != b {
+
+    if a > b {
+      a -= b
+    } else {
+      b -= a
+    }
+  }
+
+  return a
+}
+
+// determine the max execution time from all the tasks
+func maxExecutionTime(tasks Tasks) int {
+  max_execution_time := 0
+
+  for _, task := range tasks.T {
+    if task.ExecutionTime > max_execution_time {
+      max_execution_time = task.ExecutionTime
+    }
+  }
+
+  return max_execution_time
+}
+
+
+// the frame must divde evenly into the period of at least one task
+func dividesEvenly(canidate_frame int, tasks Tasks) bool {
+
+  divides_evenly := false
+
+  for _, task := range tasks.T {
+    if task.Period % canidate_frame == 0 { 
+      divides_evenly = true
+    }
+  }
+
+  return divides_evenly 
+}
+
+
+/* scheduler needs to check that jobs complete by their deadline, this means
+that there should be at least one frame boundary between the release time of a 
+job and its deadline
+*/
+func jobsCompleteByDeadline(canidate_frame int, tasks Tasks) bool {
+
+  jobs_complete := true
+
+  for _, task := range tasks.T {
+    if (2*canidate_frame - gcd(task.Period, canidate_frame)) > task.Period {
+      jobs_complete = false
+    }
+  }
+
+  return jobs_complete
+}
+
+func determineFrameLength(tasks Tasks) int {
+
+  // initalize canidate_frame to be the max of the execution times
+  canidate_frame := maxExecutionTime(tasks)
+
+  frame_divides_evenly := dividesEvenly(canidate_frame, tasks)
+  jobs_complete := jobsCompleteByDeadline(canidate_frame, tasks)
+
+  /* frame_divides_evenly is true, check if jobsCompleteByDeadline
+  if not then try another canidate up until 49 */  
+  for !(frame_divides_evenly && jobs_complete) && canidate_frame < 50 {
+    canidate_frame = canidate_frame + 1
+    frame_divides_evenly = dividesEvenly(canidate_frame, tasks)
+    jobs_complete = jobsCompleteByDeadline(canidate_frame, tasks)
+  }
+
+  if !(frame_divides_evenly && jobs_complete) {
+    // failed to find a valid frame size
+    fmt.Println("Failed to find a valid frame length. Try modifying tasks to have different execution/deadline times.")
+    os.Exit(0)
+  }
+
+  return canidate_frame
+}
+
+
+// helper function to check for errors
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func main() {
+  
+  // tasks to schedule
+  tasks := Tasks{}
+
+  // read task data from json
+  file, _ := ioutil.ReadFile("tasks.json")
+  err := json.Unmarshal([]byte(file), &tasks)
+  check(err)
+
+  // determine frame_length
+  frame_length := determineFrameLength(tasks)
+  fmt.Println(frame_length)
+}
