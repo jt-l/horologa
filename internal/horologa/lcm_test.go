4f8172de42907053c98714ec8fb79a615f6947ac
james <jamesleahy314@gmail.com>
2020-03-10 00:04:46 -0700

    refactor unit tests

--- internal/horologa/lcm_test.go
+++ internal/horologa/lcm_test.go
@@ -2,26 +2,6 @@ package horologa
 
 import "testing"
 
-func TestGcd(t *testing.T) {
-  var expectedResult int = 10
-
-  result := gcd(10, 10)
-
-  if result != expectedResult {
-    t.Fatalf("Expected %d but got %d", result, expectedResult)
-  }
-}
-
-func TestGcdOne(t *testing.T) {
-  var expectedResult int = 1
-
-  result := gcd(7, 9)
-
-  if result != expectedResult {
-    t.Fatalf("Expected %d but got %d", result, expectedResult)
-  }
-}
-
 func TestLcmSame(t *testing.T) {
   var expectedResult int = 10
 
